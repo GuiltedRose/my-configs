df648473b5fa7c11d649fc98cb482c3828ec2a1b
guiltedrose <guiltedrose@proton.me>
2024-06-28 00:19:55 -0400

    Finished arch install script

--- tools/archinstall.sh
+++ tools/archinstall.sh
@@ -31,14 +31,14 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo.env"
 
 # config setup:
-mv "$dirpath/system-startup/Pictures" ~/
-mv "$dirpath/system-startup/configs/config.conf" ~/.config/neofetch/config.conf
-mv "$dirpath/nvim ~/.config/"
-mv "$dirpath/system-startup/configs/starship.toml" ~/.config/
+mv $dirpath/system-startup/Pictures ~/
+mv $dirpath/system-startup/configs/config.conf ~/.config/neofetch/config.conf
+mv $dirpath/nvim ~/.config
+mv $dirpath/system-startup/configs/starship.toml ~/.config/
 
 # Bash setup:
-echo "alias vim=nvim" >> ~/.bashrc
+echo "alias vim='nvim'" >> ~/.bashrc
 echo "neofetch" >> ~/.bashrc
 echo "figlet V3N0M0U5" >> ~/.bashrc
-echo "'My sting is far worse than you can imagine.'" >> ~/.bashrc
+echo "echo 'My sting is far worse than you can imagine.'" >> ~/.bashrc
 eval "$(starship init bash)"
