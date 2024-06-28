0eb78964d9a6da1b8f70cd18b45de28509271235
guiltedrose <guiltedrose@proton.me>
2024-06-28 00:38:32 -0400

    Fixed it finally

--- tools/archinstall.sh
+++ tools/archinstall.sh
@@ -21,24 +21,24 @@ sudo pacman -R nano vim
 sudo pacman -S lua neovim neofetch figlet starship python nerd-fonts nodejs gcc
 
 # personal setup:
- cd  $dirpath
+ cd  $dirpath/
  git clone https://github.com/GuiltedRose/nvim.git
 
-cd
+ cd
 
 # Rust setup
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
-source "$HOME/.cargo.env"
+ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+ source "$HOME/.cargo.env"
 
 # config setup:
-mv $dirpath/system-startup/Pictures ~/
-mv $dirpath/system-startup/configs/config.conf ~/.config/neofetch/config.conf
-mv $dirpath/nvim ~/.config
-mv $dirpath/system-startup/configs/starship.toml ~/.config/
+ mv -r $dirpath/system-startup/Pictures ~/
+ mv -r $dirpath/system-startup/configs/config.conf ~/.config/neofetch/config.conf
+ mv -r $dirpath/nvim ~/.config
+ mv -r $dirpath/system-startup/configs/starship.toml ~/.config/
 
 # Bash setup:
-echo "alias vim='nvim'" >> ~/.bashrc
-echo "neofetch" >> ~/.bashrc
-echo "figlet V3N0M0U5" >> ~/.bashrc
-echo "echo 'My sting is far worse than you can imagine.'" >> ~/.bashrc
-eval "$(starship init bash)"
+ echo "alias vim='nvim'" >> ~/.bashrc
+ echo "neofetch" >> ~/.bashrc
+ echo "figlet V3N0M0U5" >> ~/.bashrc
+ echo "echo 'My sting is far worse than you can imagine.'" >> ~/.bashrc
+ echo "eval "$(starship init bash)"" >> ~/.bashrc
