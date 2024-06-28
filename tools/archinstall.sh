5a7a64b3d76a9065026ee5b5aa3f0940858cb48b
guiltedrose <guiltedrose@proton.me>
2024-06-28 06:31:06 +0500

    Fixed install scripts

--- tools/archinstall.sh
+++ tools/archinstall.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 # Variables here:
-user = "guiltedrose" # put your username here!
+user = "zelda" # put your username here!
 dirpath = "~/repos/" # this can be changed depending what directory you put your scripts into.
 
 # initial setup for AUR:
 cd /opt
-sudo git clone https://ur.archlinux.org/yay-bin.git
+sudo git clone https://aur.archlinux.org/yay-bin.git
 sudo chown -R $user:users yay-bin
 cd yay-bin
 makepkg -si
@@ -16,34 +16,29 @@ cd
 
 sudo pacman -Syu
 
-sudo pacman -R nano
-sudo pacman -R vim
+sudo pacman -R nano vim
 
-sudo pacman -S lua
-sudo pacman -S neovim
-sudo pacman -S neofetch
-sudo pacman -S figlet
-sudo pacman -S starship
-sudo pacman -S python # make sure it's updated before doing a system update.
+sudo pacman -S lua neovim neofetch figlet starship python
 
 # personal setup:
-cd $dirpath
-git clone https://github.com/GuiltedRose/nvim.git
+ cd  $dirpath
+ git clone https://github.com/GuiltedRose/nvim.git
 
 cd
-
 
 # Rust setup
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 source "$HOME/.cargo.env"
+
 # config setup:
-mv $dirpath/system-startup/Pictures ~/
-mv $dirpath/system-startup/configs/config.conf ~/.config/neofetch/config.conf
-mv $dirpath/nvim ~/.config/
-mv $dirpath/system-startup/configs/starship.toml ~/.config/
+mv "$dirpath/system-startup/Pictures" ~/
+mv "$dirpath/system-startup/configs/config.conf" ~/.config/neofetch/config.conf
+mv "$dirpath/nvim ~/.config/"
+mv "$dirpath/system-startup/configs/starship.toml" ~/.config/
+
 # Bash setup:
 echo "alias vim=nvim" >> ~/.bashrc
 echo "neofetch" >> ~/.bashrc
 echo "figlet V3N0M0U5" >> ~/.bashrc
-echo "echo "Mysting is far worse than you can imagine."" >> ~/.bashrc
+echo "My sting is far worse than you can imagine." >> ~/.bashrc
 eval "$(starship init bash)"
